8d46ed0104a5e81d5c0430f8d3602a5e318fb4a0
Elajsen <eliasmattson@Eliass-MBP.home>
2022-11-21 19:09:37 +0100

    Added buying and selling

--- mongo_handler.py
+++ mongo_handler.py
@@ -8,15 +8,37 @@ class MongoHandler:
         conn_str = "mongodb+srv://eliasmattson:Qtepa112@kafka-mongo.lfvc3bl.mongodb.net/?retryWrites=true&w=majority"
         self.client = pymongo.MongoClient(conn_str)
 
-    def insert_value(self, db, collection, value):
-        assert isinstance(value, list), "Values must be in list"
-        assert isinstance(value[0], dict), "Items must be dict"
+    def insert_value(self, db, collection, values):
+        assert isinstance(values, list), "Values must be in list"
+        assert isinstance(values[0], dict), "Items must be dict"
         try:
-            self.client[db][collection].insert_many([value])
+            self.client[db][collection].insert_many(values)
             print("Item inserted")
         except Exception as e:
             print("Couldn't insert item")
             print(str(e))
+
+    def sell_items(self, db, collection, object_ids, selling_price):
+        objects_to_delete = []
+        for object_id in object_ids:
+            objects_to_delete.append(self.search_collection(db=db,
+                                                            collection=collection,
+                                                            filter={"_id": object_id}))
+        self.delete_values(objects_to_delete)
+
+    def delete_values(self, db, collection, objects):
+        assert len(objects) > 0, "Object ids list is empty..."
+
+        try:
+            self.client[db][collection].delete_many(objects)
+            print(f"Deleted {len(objects)} items.")
+        except Exception as e:
+            print("Couldn't delete items...")
+            print(f"{str(e)}")
+
+    def search_collection(self, db, collection, filter):
+        res = self.client[db][collection].find(filter)
+        return res
 
     def list_databases(self):
         # Return the names of the databases
--- rabbitmq_handler.py
+++ rabbitmq_handler.py
@@ -1,19 +1,25 @@
 from mongo_handler import MongoHandler
 import pika, sys, os, json
+from datetime import datetime
 
 
 class RabbitHandler:
     def __init__(self):
         # Initialize the mongodb
-        mongo_handler = MongoHandler()
+        self.mongo_handler = MongoHandler()
+        self.IP = "localhost"
+        self.queue = "trading_bot_backend"
+
+        self.trading_db = "trading_db"
+        self.holdings_collection = "holdings"
 
         # Declare the queuee
-        connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(self.IP))
         channel = connection.channel()
 
-        channel.queue_declare(queue="hello")
+        channel.queue_declare(queue=self.queue)
 
-        channel.basic_consume(queue="hello",
+        channel.basic_consume(queue=self.queue,
                               auto_ack=True,
                               on_message_callback=self.callback)
 
@@ -22,8 +28,31 @@ class RabbitHandler:
 
     def callback(self, ch, method, properties, serialized_body):
         body = json.loads(serialized_body)
-        print(f" [x] Received {body}")
+        type = body.get("type")
+        res = getattr(self, type)(selling_price=body.get("price"),
+                                  objects=body.object_ids)
 
+    def buy(self, ticker, amount, price):
+        print("Buying")
+        values = [{
+            "time": datetime.now(),
+            "ticker": ticker,
+            "amount": amount,
+            "price": price
+        }]
+
+        self.mongo_handler.insert_value(db=self.trading_db,
+                                        collection=self.holdings_collection,
+                                        values=values)
+
+    def sell(self, selling_price, objects):
+        print("Selling")
+        assert isinstance(objects, list), "Objects needs to be a list of objects to sell"
+
+        self.mongo_handler.sell_items(db=self.trading_db,
+                                      collection=self.holdings_collection,
+                                      object_ids=objects,
+                                      selling_price=selling_price)
 
 if __name__ == '__main__':
     try:
